ca9f8de42c4dbd6bb66f805b9d3393cfb832a373
4Quark <mariya_baranova@list.ru>
2023-11-01 01:23:21 +0300

    refactor: remove commit

--- RSS-React/src/services/ErrorBoundary.tsx
+++ RSS-React/src/services/ErrorBoundary.tsx
@@ -18,28 +18,22 @@ class ErrorBoundary extends React.Component<Props, State> {
   }
 
   public componentDidCatch(error: Error) {
-    console.error(`[Error Boundary] caught error: ${error.message}`);
     console.error('ErrorBoundary:', error);
   }
-  // public componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-  //   console.error('Uncaught error:', error, errorInfo);
-  // }
 
   public render() {
     if (this.state.hasError) {
       return (
         <section>
-          <div className="container column">
-            <h2>Sorry... there was an error</h2>
-            <button
-              className="reload_button"
-              onClick={() => {
-                location.reload();
-              }}
-            >
-              Refresh page
-            </button>
-          </div>
+          <h2>Sorry... there was an error</h2>
+          <button
+            className="reload_button"
+            onClick={() => {
+              location.reload();
+            }}
+          >
+            Refresh page
+          </button>
         </section>
       );
     }
