ebf9b8f95cd46964ca61d4edcb63f873a759e87d
4Quark <mariya_baranova@list.ru>
2023-11-10 17:09:13 +0300

    feat: add Context

--- RSS-React/src/components/SearchBar.tsx
+++ RSS-React/src/components/SearchBar.tsx
@@ -1,17 +1,19 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useContext, useEffect } from 'react';
 import ErrorButton from './errorBtn';
 import { useNavigate } from 'react-router-dom';
+import { SearchContext } from '../services/context';
 
 type myProps = { fetchData: () => void };
 
 function SearchCard(props: myProps) {
-  const [value, setValue] = useState('');
+  const { setValue } = useContext(SearchContext);
+  const { value } = useContext(SearchContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     const localValue: string = localStorage.getItem('searchInput') || '';
     setValue(localValue);
-  }, []);
+  }, [setValue]);
 
   const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,7 +29,7 @@ function SearchCard(props: myProps) {
           className="search_input"
           type="text"
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setValue(e.target.value.trim())}
         />
         <button type="submit" className="search_btn">
           Search
